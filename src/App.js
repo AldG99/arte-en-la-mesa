c5152147167fcfa5a0f7b81c1e76090986563f3e
Alfredo <garciaglz17@outlook.es>
2023-09-14 19:23:25 -0600

    Diseño y nueva estructura del código

--- src/App.js
+++ src/App.js
@@ -17,12 +17,12 @@ function AlbumDeImagenes() {
 
   return (
     <Carousel
-  showThumbs={false} // Configuración para ocultar los thumbs (puntos)
-  showStatus={false}
-  infiniteLoop
-  autoPlay
-  className="custom-carousel" // Utiliza la clase personalizada
->
+      showThumbs={false} // Configuración para ocultar los thumbs (puntos)
+      showStatus={false}
+      infiniteLoop
+      autoPlay
+      className="custom-carousel" // Utiliza la clase personalizada
+    >
       {imagenesDelAlbum.map((imagen, index) => (
         <div key={imagen.id}>
           <img
@@ -39,6 +39,7 @@ function AlbumDeImagenes() {
 }
 
 // Función para obtener el nombre del mes en base al índice
+
 function obtenerMes(index) {
   const meses = [
     'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio',
@@ -96,6 +97,86 @@ function App() {
               <p style={{ userSelect: 'none' }}>¡Embárcate en un viaje de sabores deliciosos!</p>
             </div>
           </div>
+          <div className='ornament-pan-uno' style={{ userSelect: 'none' }}>
+            <img
+              src={require('./images/pan-dibujo-3.png')}
+              alt='Decorado Olas'
+              onContextMenu={(e) => e.preventDefault()}
+              onDragStart={(e) => e.preventDefault()}
+            />
+          </div>
+          <div className='ornament-pan-dos' style={{ userSelect: 'none' }}>
+            <img
+              src={require('./images/pan-dibujo-5.png')}
+              alt='Decorado Olas'
+              onContextMenu={(e) => e.preventDefault()}
+              onDragStart={(e) => e.preventDefault()}
+            />
+          </div>
+          <div className='ornament-pan-tres' style={{ userSelect: 'none' }}>
+            <img
+              src={require('./images/pan-dibujo-6.png')}
+              alt='Decorado Olas'
+              onContextMenu={(e) => e.preventDefault()}
+              onDragStart={(e) => e.preventDefault()}
+            />
+          </div>
+          <div className='ornament-pan-cuatro' style={{ userSelect: 'none' }}>
+            <img
+              src={require('./images/pan-dibujo-8.png')}
+              alt='Decorado Olas'
+              onContextMenu={(e) => e.preventDefault()}
+              onDragStart={(e) => e.preventDefault()}
+            />
+          </div>
+          <div className='ornament-pan-cinco' style={{ userSelect: 'none' }}>
+            <img
+              src={require('./images/pan-dibujo-1.png')}
+              alt='Decorado Olas'
+              onContextMenu={(e) => e.preventDefault()}
+              onDragStart={(e) => e.preventDefault()}
+            />
+          </div>
+          <div className='ornament-pan-seis' style={{ userSelect: 'none' }}>
+            <img
+              src={require('./images/pan-dibujo-2.png')}
+              alt='Decorado Olas'
+              onContextMenu={(e) => e.preventDefault()}
+              onDragStart={(e) => e.preventDefault()}
+            />
+          </div>
+          <div className='ornament-pan-siete' style={{ userSelect: 'none' }}>
+            <img
+              src={require('./images/pan-dibujo-4.png')}
+              alt='Decorado Olas'
+              onContextMenu={(e) => e.preventDefault()}
+              onDragStart={(e) => e.preventDefault()}
+            />
+          </div>
+          <div className='ornament-pan-ocho' style={{ userSelect: 'none' }}>
+            <img
+              src={require('./images/pan-dibujo-7.png')}
+              alt='Decorado Olas'
+              onContextMenu={(e) => e.preventDefault()}
+              onDragStart={(e) => e.preventDefault()}
+            />
+          </div>
+          <div className='ornament-flecha' style={{ userSelect: 'none' }}>
+            <img
+              src={require('./images/flecha-dibujo.png')}
+              alt='Decorado Olas'
+              onContextMenu={(e) => e.preventDefault()}
+              onDragStart={(e) => e.preventDefault()}
+            />
+          </div>
+          <div className='ornament-panaderia-sello' style={{ userSelect: 'none' }}>
+            <img
+              src={require('./images/panaderia-sello.png')}
+              alt='Decorado Olas'
+              onContextMenu={(e) => e.preventDefault()}
+              onDragStart={(e) => e.preventDefault()}
+            />
+          </div>
         </div>
       </div>
       <section id="preparar-pan">
@@ -107,98 +188,94 @@ function App() {
         </ul>
       </section>
       <section id="nuestra-historia" class="flex-container">
-  <div class="text-container">
-    <h2>Nuestra Historia</h2>
-    <p>Hace más de una década, un grupo de amigos con un amor compartido por la panadería decidió convertir su pasión en una realidad palpable. Así nació nuestra panadería en horno, un lugar donde la tradición y la creatividad se entrelazan para crear experiencias gastronómicas inolvidables.</p>
-    <p>Desde nuestros humildes comienzos en un pequeño local, hemos estado comprometidos con la calidad y la autenticidad en cada producto que sale de nuestro horno. Cada mañana, cuando el sol apenas se asoma en el horizonte, nuestros panaderos comienzan su labor meticulosa, seleccionando los ingredientes más frescos y preparando la masa con dedicación artesanal.</p>
-    <p>La esencia de nuestra panadería radica en la comunidad que nos rodea. Hemos tenido el honor de servir a generaciones de clientes que comparten nuestro amor por el pan recién horneado y los pasteles con un toque casero. Cada visita es una oportunidad para celebrar los pequeños momentos de la vida con una deliciosa recompensa.</p>
-    <p>Pero más allá de la calidad de nuestros productos, lo que realmente nos distingue es el amor con el que hacemos lo que hacemos. Cada pieza que sale de nuestro horno está impregnada de ese amor y dedicación, y eso es lo que nos impulsa a seguir horneando día tras día.</p>
-    </div>
-  <div class="image-container">
-    <img src={require('./images/foto_ana.jpg')} alt='Foto de Ana' />
-    <p>Ana</p>
-  </div>
-</section>
+        <div class="text-container">
+          <h2>Nuestra Historia</h2>
+          <p>Hace más de una década, un grupo de amigos con un amor compartido por la panadería decidió convertir su pasión en una realidad palpable. Así nació nuestra panadería en horno, un lugar donde la tradición y la creatividad se entrelazan para crear experiencias gastronómicas inolvidables.</p>
+          <p>Desde nuestros humildes comienzos en un pequeño local, hemos estado comprometidos con la calidad y la autenticidad en cada producto que sale de nuestro horno. Cada mañana, cuando el sol apenas se asoma en el horizonte, nuestros panaderos comienzan su labor meticulosa, seleccionando los ingredientes más frescos y preparando la masa con dedicación artesanal.</p>
+          <p>La esencia de nuestra panadería radica en la comunidad que nos rodea. Hemos tenido el honor de servir a generaciones de clientes que comparten nuestro amor por el pan recién horneado y los pasteles con un toque casero. Cada visita es una oportunidad para celebrar los pequeños momentos de la vida con una deliciosa recompensa.</p>
+          <p>Pero más allá de la calidad de nuestros productos, lo que realmente nos distingue es el amor con el que hacemos lo que hacemos. Cada pieza que sale de nuestro horno está impregnada de ese amor y dedicación, y eso es lo que nos impulsa a seguir horneando día tras día.</p>
+        </div>
+        <div class="image-container">
+          <img src={require('./images/foto_ana.jpg')} alt='Foto de Ana' />
+          <p>Ana</p>
+        </div>
+      </section>
       <section id="productos">
-  <h2>Nuestros Panes Horneados del Mes</h2>
-  <div className='imagen-mes'>
-  <AlbumDeImagenes />
-  </div>
-</section>
+        <h2>Nuestros Panes Horneados del Mes</h2>
+        <div className='imagen-mes'>
+          <AlbumDeImagenes />
+        </div>
+      </section>
       <section id="ordenar-ahora">
-  <h2>Ordenar Ahora</h2>
-  <p>No esperes más para disfrutar de nuestros deliciosos productos horneados. Haz tu pedido ahora y deleita a tus papilas gustativas con auténticos sabores horneados.</p>
-  <p>Ofrecemos opciones de entrega y retiro en tienda para tu comodidad.</p>
-  <div class="subscription-buttons">
-    <div class="subscription-option">
-    <img
-            src={require('./images/pan_1.png')}
-            alt='Una Amiga y Un Amigo'
-            onContextMenu={(e) => e.preventDefault()}
-            onDragStart={(e) => e.preventDefault()}
-          />
-      <h3>3 meses</h3>
-      <p>$200</p>
-      <button>Ordenar Ahora</button>
-    </div>
-    <div class="subscription-option">
-    <img
-            src={require('./images/pan_2.png')}
-            alt='Una Amiga y Un Amigo'
-            onContextMenu={(e) => e.preventDefault()}
-            onDragStart={(e) => e.preventDefault()}
-          />
-      <h3>6 meses</h3>
-      <p>$350</p>
-      <button>Ordenar Ahora</button>
-    </div>
-    <div class="subscription-option">
-    <img
-            src={require('./images/pan_3.png')}
-            alt='Una Amiga y Un Amigo'
-            onContextMenu={(e) => e.preventDefault()}
-            onDragStart={(e) => e.preventDefault()}
-          />
-      <h3>12 meses</h3>
-      <p>$600</p>
-      <button>Ordenar Ahora</button>
-    </div>
-  </div>
-</section>
+        <h2>Ordenar Ahora</h2>
+        <p>No esperes más para disfrutar de nuestros deliciosos productos horneados. Haz tu pedido ahora y deleita a tus papilas gustativas con auténticos sabores horneados.</p>
+        <p>Ofrecemos opciones de entrega y retiro en tienda para tu comodidad.</p>
+        <div class="subscription-buttons">
+          <div class="subscription-option">
+            <img
+              src={require('./images/pan_1.png')}
+              alt='Una Amiga y Un Amigo'
+              onContextMenu={(e) => e.preventDefault()}
+              onDragStart={(e) => e.preventDefault()}
+            />
+            <h3>3 meses</h3>
+            <p>$200</p>
+            <button>Ordenar Ahora</button>
+          </div>
+          <div class="subscription-option">
+            <img
+              src={require('./images/pan_2.png')}
+              alt='Una Amiga y Un Amigo'
+              onContextMenu={(e) => e.preventDefault()}
+              onDragStart={(e) => e.preventDefault()}
+            />
+            <h3>6 meses</h3>
+            <p>$350</p>
+            <button>Ordenar Ahora</button>
+          </div>
+          <div class="subscription-option">
+            <img
+              src={require('./images/pan_3.png')}
+              alt='Una Amiga y Un Amigo'
+              onContextMenu={(e) => e.preventDefault()}
+              onDragStart={(e) => e.preventDefault()}
+            />
+            <h3>12 meses</h3>
+            <p>$600</p>
+            <button>Ordenar Ahora</button>
+          </div>
+        </div>
+      </section>
       <section id="contacto">
         <h2>Contacto</h2>
-        {/* Agrega imágenes relacionadas con la ubicación o el contacto */}
-        {/* Ejemplo: <img src="imagen_mapa.jpg" alt="Ubicación de la panadería" /> */}
         <p>¿Tienes alguna pregunta o comentario? ¡No dudes en ponerte en contacto con nosotros!</p>
         <p>Estamos ubicados en 123 Calle Principal, Ciudad, País.</p>
         <p>Puedes llamarnos al +1 (123) 456-7890.</p>
         <p>Envíanos un correo electrónico a info@tu-panaderia.com.</p>
-        
         {/* Agrega los iconos de redes sociales */}
         <div className="social-icons">
-        <a href="https://github.com/AldG99/arte-en-la-mesa" className="social-icon" style={{ color: '#fff' }}>
-  <FaFacebook />
-</a>
-<a href="https://github.com/AldG99/arte-en-la-mesa" className="social-icon" style={{ color: '#fff' }}>
-  <FaInstagram />
-</a>
-<a href="https://github.com/AldG99/arte-en-la-mesa" className="social-icon" style={{ color: '#fff' }}>
-  <FaTwitter />
-</a>
-<a href="https://github.com/AldG99/arte-en-la-mesa" className="social-icon" style={{ color: '#fff' }}>
-  <FaGooglePlus />
-</a>
+          <a href="https://github.com/AldG99/arte-en-la-mesa" className="social-icon" style={{ color: '#fff' }}>
+            <FaFacebook />
+          </a>
+          <a href="https://github.com/AldG99/arte-en-la-mesa" className="social-icon" style={{ color: '#fff' }}>
+            <FaInstagram />
+          </a>
+          <a href="https://github.com/AldG99/arte-en-la-mesa" className="social-icon" style={{ color: '#fff' }}>
+            <FaTwitter />
+          </a>
+          <a href="https://github.com/AldG99/arte-en-la-mesa" className="social-icon" style={{ color: '#fff' }}>
+            <FaGooglePlus />
+          </a>
         </div>
       </section>
       <footer>
-  <div className="gi-knife-fork">
-    <GiKnifeFork />
-  </div>
-  <div className='copyright'>
-    © 2023 Alfredo García. Todos los derechos reservados.
-  </div>
-</footer>
-
+        <div className="gi-knife-fork">
+          <GiKnifeFork />
+        </div>
+        <div className='copyright'>
+          © 2023 Alfredo García. Todos los derechos reservados.
+        </div>
+      </footer>
     </div>
   );
 }
